aeca65a0222822268e9d2f7b3ca439ce520b2980
thorstu22 <99290893+thorstu22@users.noreply.github.com>
2022-08-29 16:22:31 +0100

    Initial commit from gatsby

---
+++ src/pages/SDK.test.js
@@ -0,0 +1,14 @@
+// import { render, screen } from '@testing-library/react';
+// import SDK-list from './SDK-list';
+
+// test('renders list of installed SDKs', () => {
+//   render(<SDK-list />);
+//   const linkElement = screen.getByText(/installed/i);
+//   expect(arrayElement).toBeInTheDocument();
+// });
+
+// test('renders list of uninstalled SDK’s when uninstalled is clicked', () => {
+//   render(<SDK-list />);
+//   const linkElement = screen.getByText(/uninstalled/i);
+//   expect(arrayElement).toBeInTheDocument();
+// });
